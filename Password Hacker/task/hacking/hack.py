c278c0f8d22d6fd43f317d2213769868031273db
Didi <ofisser86@gmail.com>
2020-10-27 18:06:13 +0200

    Complete Stage 3/5

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -2,10 +2,10 @@ import sys
 import itertools
 from socket import socket
 
-alpha = [chr(i) for i in range(97, 123)] + [chr(i) for i in range(48, 58)]
 
 with socket() as client:
-
+    with open('passwords.txt', 'r') as f:
+        f.readlines()
     host = sys.argv[1]
     port = int(sys.argv[2])
     # data = sys.argv[3]
@@ -13,14 +13,19 @@ with socket() as client:
     address = (host, port)
     client.connect(address)
     i = 1
-    while True:
-        x = itertools.product(alpha, repeat=i)
-        for j in range(len(alpha) ** i):
-            try_pass = "".join((next(x)))
-            data = try_pass.encode()
-            client.send(data)
-            response = client.recv(1024)
-            if response.decode() == 'Connection success!':
-                print(data.decode())
-                sys.exit()
-        i = i + 1
+    with open('passwords.txt', 'r') as f:
+        password = f.readlines()
+        l_pass = []
+        for i in password:
+            l_pass.append(i.replace('\n', ''))
+        while True:
+            for j in l_pass:
+                x = map(''.join, itertools.product(*((c.upper(), c.lower()) for c in j)))
+                for m in x:
+                    data = m.encode()
+                    client.send(data)
+                    response = client.recv(1024)
+                    if response.decode() == 'Connection success!':
+                        print(data.decode())
+                        sys.exit()
+            i = i + 1
